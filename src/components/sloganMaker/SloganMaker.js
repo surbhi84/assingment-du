0a20839afc6f03bc28f681c50555a1ed9882c54a
Surbhi-dukaan <surbhi.kukreti@rankz.io>
2023-08-27 14:41:43 +0530

    add: copy feature

--- src/components/sloganMaker/SloganMaker.js
+++ src/components/sloganMaker/SloganMaker.js
@@ -1,6 +1,48 @@
 import React, { useState } from "react";
 import { slogans } from "../slogans";
 
+const SloganBlock = ({ item, index }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [ifCopied, setIfCopied] = useState(false);
+
+  return (
+    <div
+      className='sm-slogan-tile flex-center'
+      key={item.id}
+      onClick={() => {
+        navigator.clipboard.writeText(item.text);
+        setIfCopied(true);
+      }}
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => {
+        setShowTooltip(false);
+        setIfCopied(false);
+      }}
+    >
+      {item.text}
+      {showTooltip && ifCopied ? (
+        <div
+          className={`sm-tooltip ${
+            index % 2 === 0 ? "sm-copied" : "sm-copied-1"
+          } `}
+        >
+          Copied!
+        </div>
+      ) : (
+        showTooltip && (
+          <div
+            className={`sm-tooltip ${
+              index % 2 === 0 ? "sm-copy" : "sm-copy-1"
+            }`}
+          >
+            Click to copy
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
 export const SloganMaker = ({ slogansNum = "1,023" }) => {
   const [slogan, setSlogan] = useState("cozy");
 
@@ -19,7 +61,7 @@ export const SloganMaker = ({ slogansNum = "1,023" }) => {
           value={slogan}
         />
         <img
-          src='./icons/cross.png'
+          src='./icons/cross.svg'
           className='sm-input-cross'
           alt='cross-icon'
           onClick={() => setSlogan("")}
@@ -32,29 +74,23 @@ export const SloganMaker = ({ slogansNum = "1,023" }) => {
         <button className='sm-btn-outline'>Download all</button>
       </div>
       <div className='slogan-container'>
-        {slogans.map((item) => {
-          return (
-            <div className='sm-slogan-tile flex-center' key={item.id}>
-              {item.text}
-            </div>
-          );
-        })}
+        {slogans.map((item, index) => (
+          <SloganBlock item={item} index={index} />
+        ))}
       </div>
       <div className='sm-partition-1'></div>
       {/* pagination */}
 
       <div className='pagination-div flex-center'>
-          <div className="current-page">
-            1
-          </div>
-          <div>2</div>
-          <div>3</div>
-          <div>...</div>
-          <div>21</div>
-          <button className='flex-center pagination-btn'>
-            Next <img src='/icons/arrow.png' alt='arrow_icon'/>
-          </button>
-        </div>
+        <div className='current-page'>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>...</div>
+        <div>21</div>
+        <button className='flex-center pagination-btn'>
+          Next <img src='/icons/arrow.svg' alt='arrow_icon' />
+        </button>
+      </div>
     </div>
   );
 };
