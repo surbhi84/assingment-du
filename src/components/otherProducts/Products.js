051261f16cd2ac5c62e2675bdd97d2bec674cdee
Surbhi-dukaan <surbhi.kukreti@rankz.io>
2023-08-27 14:40:26 +0530

    update: svg url

--- src/components/otherProducts/Products.js
+++ src/components/otherProducts/Products.js
@@ -2,28 +2,28 @@ import React from "react";
 
 const thumbnails = [
   {
-    src: "/images/ppg.png",
+    src: "/images/ppg.svg",
     alt: "privacy_policy_generator",
     heading: "Privacy Policy Generator",
     text: "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
   },
 
   {
-    src: "/images/tnc.png",
+    src: "/images/tnc.svg",
     alt: "terms_and_conditions_generator",
     heading: "Terms & Conditions Generator",
     text: "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
   },
 
   {
-    src: "/images/dng.png",
+    src: "/images/dng.svg",
     alt: "domain_name_generator",
     heading: "Domain Name Generator",
     text: "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
   },
 
   {
-    src: "/images/ig.png",
+    src: "/images/ig.svg",
     alt: "invoice_generator",
     heading: "Invoice Generator",
     text: "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
@@ -33,7 +33,7 @@ const thumbnails = [
 export const Products = () => {
   return (
     <div className='pr-main'>
-      <h2 className='pr-heading'>Try our other free products</h2>
+      <div className='pr-heading'>Try our other free products</div>
       <div className='pr-container'>
         {thumbnails.map((item) => (
           <div className='pr-item-div'>
